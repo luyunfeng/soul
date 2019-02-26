2f8facb75137e1b8aa5cf69786e33b075427b34d
lucode <yunfeng.lu@Bangdao-tech.com>
2019-02-27 00:07:35 +0800

    项目初始化搭建

---
+++ soul-example/soul-springboot/src/main/java/tech/soulcoder/example/springboot/Application.java
@@ -0,0 +1,15 @@
+package tech.soulcoder.example.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author yunfeng.lu
+ * @create 2019/2/26.
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
+    }
+}
