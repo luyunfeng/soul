a37079d6d964a4e0d329f87e179bbfe95745e885
ruanzhi <1655362684@qq.com>
2019-03-17 11:08:32 +0800

    对于hystrix学习

--- soul-example/soul-eureka-order-service/src/main/java/tech/soulcoder/order/EurekaOrderServiceApplication.java
+++ soul-example/soul-eureka-order-service/src/main/java/tech/soulcoder/order/EurekaOrderServiceApplication.java
@@ -2,7 +2,9 @@ package tech.soulcoder.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
@@ -14,6 +16,8 @@ import org.springframework.web.client.RestTemplate;
  */
 @SpringBootApplication
 @EnableFeignClients
+@EnableCircuitBreaker
+@EnableHystrixDashboard
 public class EurekaOrderServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(EurekaOrderServiceApplication.class, args);
